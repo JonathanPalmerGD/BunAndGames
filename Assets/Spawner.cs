1fd55bb655aed07a57528a0eafcb9e22d9cf7678
unknown <jkp2377@GDD-8.main.ad.rit.edu>
2015-04-09 13:58:55 -0400

    Initial Project Commit! HOORAH BUNNIES!

---
+++ Assets/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour 
+{
+	public GameObject prefab;
+	public int counter = 0;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.Q))
+		{
+			counter++;
+
+			Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0);
+
+			GameObject.Instantiate(prefab, pos, Quaternion.identity);
+		}
+
+		if(Input.GetKey(KeyCode.T))
+		{
+			for(int i = 0; i < 15; i++)
+			{
+				counter++;
+				
+				Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0);
+				
+				GameObject.Instantiate(prefab, pos, Quaternion.identity);
+			}
+		}
+
+		
+		if(Input.GetKey(KeyCode.E))
+		{
+			Debug.Log("There are currently: " + counter + " sprites\n"); 
+		}
+	}
+}
