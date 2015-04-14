75c8722caac0f1c7a60c10c37e53dffecca49107
Jonathan Palmer <Jon@JonathanPalmerGD.com>
2015-04-14 13:25:25 -0400

    Bunny fixing

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -1,32 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharacterController : MonoBehaviour {
-
+public class CharacterController : MonoBehaviour 
+{
 	public float speed;
 	public Vector3 targetPosition;
 	private Vector3 clickPos;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-
+	void Update () 
+	{
 		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * speed);
 
 		clickPos = Input.mousePosition;
 
-
-
-		if (Input.GetMouseButton (0)){
-
+		if (Input.GetMouseButton (0))
+		{
 			clickPos.z = 10.0f;
 
-			RaycastHit hit;
 			Vector3 targPoint = Camera.main.ScreenToWorldPoint (clickPos); //mousePosition returns Vector3 position based on screen width and heigh. 0,0 is bottom left corner.
 			//Debug.DrawLine(Vector3.zero, targPoint, Color.white, 15.0f);
 			Vector3 test = new Vector3((-Input.mousePosition.x - Screen.width/2), (Input.mousePosition.y - Screen.height/2), transform.position.z);
