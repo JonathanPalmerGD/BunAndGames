5525657d060e2e8a8e94d364f3c790a71be01d4e
Argzero <forrest.z.shooster@gmail.com>
2015-05-17 14:09:34 -0400

    Made Bubble beautiful! :D

---
+++ Assets/Resources/BubbleShader.shader
@@ -0,0 +1,52 @@
+﻿Shader "Bubble" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_SubTex ("Sub-texture (RGB)", 2D) = "white" {}
+		_EmisColor ("Emissive Color", Color) = (0.2,0.2,0.2,0)
+		_Color ("Multiply Color", Color) = (0,0,0)
+	}
+	SubShader {
+		Tags { "LIGHTMODE"="Vertex" "QUEUE"="Transparent" "IGNOREPROJECTOR"="true" "RenderType"="Transparent" }
+		LOD 200
+		
+		Lighting Off
+		Material {
+			Emission [_EmisColor]
+		}
+		ZWrite Off
+		Cull Off
+		Pass {
+			Tags { "LIGHTMODE"="Vertex" "QUEUE"="Transparent" "IGNOREPROJECTOR"="true" "RenderType"="Transparent" }
+			Lighting Off
+			Material {
+				Emission [_EmisColor]
+			}
+			ZWrite Off
+			Cull Off
+			Blend SrcAlpha OneMinusSrcAlpha
+			AlphaTest Greater 0.001
+			ColorMask RGB
+			ColorMaterial AmbientAndDiffuse
+			SetTexture [_MainTex] { combine primary * texture }
+		}
+		CGPROGRAM
+		#pragma surface surf Lambert alpha
+
+		sampler2D _MainTex;
+		sampler2D _SubTex;
+		float3 _Color;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			half4 c = tex2D (_MainTex, IN.uv_MainTex);
+			half4 s = tex2D (_SubTex, IN.uv_MainTex);
+			o.Albedo = s.rgb*_Color;
+			o.Alpha = c.a;
+		}
+		ENDCG
+	} 
+	FallBack "Particles/Additive(Soft)"
+	}
--- Assets/Scripts/PaintBlob.cs
+++ Assets/Scripts/PaintBlob.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class PaintBlob : MonoBehaviour {
+    public static Color PaintColor = Color.cyan;
 	public bool Active;
 	public Vector3 Target;
 	public float ScaleMultiplier = 1;
@@ -20,29 +21,31 @@ public class PaintBlob : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        gameObject.renderer.material.color = PaintColor;
 		scale = gameObject.transform.localScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //gameObject.renderer.material.color = Color.Lerp(gameObject.renderer.material.color, PaintColor,0.1f)
 		if (state == BState.Done) {
 			Destroy(gameObject);
 		}
 		if (state == BState.Approaching) {
 			ScaleMultiplier = 1;
-			gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, Target, 0.75f);
+			gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, Target, 0.35f);
 			if ((gameObject.transform.position - Target).magnitude == 0) { state = BState.Splat; }
 		}
 		if (state == BState.Splat) {
-			ScaleMultiplier = 1 + (0.5f * UPPERLIMIT(SplatFrame, 2f));
-			if (SplatFrame > 120) { 
+			ScaleMultiplier = 1 + (1.5f * UPPERLIMIT(SplatFrame, 2f));
+			if (SplatFrame > 40) { 
 				SplatFrame = 0;
 				state = BState.Done;
 			}
 
 			SplatFrame++;
 		}
-		gameObject.transform.localScale = Vector3.Lerp(gameObject.transform.localScale, scale * ScaleMultiplier, 0.01f);
+		gameObject.transform.localScale = Vector3.Lerp(gameObject.transform.localScale, scale * ScaleMultiplier, 0.1f);
 	}
 
 	public void Approach(Vector3 _target) {
